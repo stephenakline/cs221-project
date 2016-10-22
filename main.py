20cefcbc9300d6fd145c1bf157aa9dd86f43f06d
Stephen Kline <sakline@stanford.edu>
2016-10-21 18:44:17 -0700

    adding menu for user to easily interact with various play scenarios.

--- main.py
+++ main.py
@@ -2,11 +2,110 @@ import sys
 import rpsBots
 import simulation
 
-# if __name__ == '__main__':
-probs = {'rock': .33, 'paper': .33, 'scissor': (1-.33-.33)}
-bot1 = rpsBots.ProbabilityRPSBot(probs)
+'''
+TODO: human can play a bot
+      human can enter probabilities for their own bot
+      add other bots
+'''
 
-probs = {'rock': 0.50, 'paper': 0.50, 'scissor': 0.0}
-bot2 = rpsBots.OrderRPSBot(['rock', 'paper', 'scissor'])
+BOTS = {}
+BOTS['bot-basic'] = rpsBots.ProbabilityRPSBot({'rock': .33, 'paper': .33, 'scissor': (1-.33-.33)})
+BOTS['bot-random'] = rpsBots.ProbabilityRPSBot({'rock': .5, 'paper': .2, 'scissor': (1-.5-.2)})
+# probs = {'rock': 0.50, 'paper': 0.50, 'scissor': 0.0}
+# bot2 = rpsBots.OrderRPSBot(['rock', 'paper', 'scissor'])
+#
+# game = simulation.Simulation(bot1, bot2, 10000)
 
-game = simulation.Simulation(bot1, bot2, 10000)
+# REPL and main entry point
+def repl(command=None):
+    '''REPL: read, evaluate, print, loop'''
+
+    while True:
+        sys.stdout.write('>> ')
+        line = sys.stdin.readline().strip()
+        if not line:
+            break
+
+        if command is None:
+            cmdAndLine = line.split(None, 1)
+            cmd, line = cmdAndLine[0], ' '.join(cmdAndLine[1:])
+        else:
+            cmd = command
+            line = line
+
+        print ''
+
+        if cmd == 'help':
+            print 'Usage: <command> [arg1, arg2, ...]'
+            print ''
+            print 'Commands:'
+            print '\n'.join(a + '\t\t\t' + b for a, b in [
+                ('bot-basic', 'Bot who plays the uniform frequencies'),
+                ('bot-random', 'Bot who plays with random frequencies'),
+                ('bot-user', 'Bot who is defined by the user'),
+                ('bot-oracle', 'Bot who knows the opponent\'s strategy'),
+                ('simulate [bot1] [bot2] [number of rounds]', 'Simulate two bots playing'),
+                ('single [bot1] [bot2]', 'Simulate 1 round between two bots'),
+                ('user [bot1]', 'User plays against a bot'),
+            ])
+            print ''
+            print 'Enter empty line to quit'
+
+        elif cmd == 'single':
+            # call single bot play
+            if len(line.split()) != 2:
+                print 'Need to provide two bots to play'
+                print '\tUsage: single [bot1] [bot2]'
+                print ''
+            else:
+                name1, name2 = line.split()
+                bot1 = BOTS[name1]
+                bot2 = BOTS[name2]
+                game = simulation.Simulation(bot1, bot2)
+                game.singleGame()
+
+        elif cmd == 'simulate':
+            if len(line.split()) != 3:
+                print 'Need to provide two bots to play:'
+                print '\tUsage: single [bot1] [bot2] [number of rounds]'
+                print ''
+            else:
+                name1, name2, rounds = line.split()
+                bot1 = BOTS[name1]
+                bot2 = BOTS[name2]
+                game = simulation.Simulation(bot1, bot2)
+                game.simulate(int(rounds))
+                print(game)
+
+        # elif cmd == 'both':
+        #     line = wordsegUtil.cleanLine(line)
+        #     smoothCost = wordsegUtil.smoothUnigramAndBigram(unigramCost, bigramCost, 0.2)
+        #     parts = [wordsegUtil.removeAll(w, 'aeiou') for w in wordsegUtil.words(line)]
+        #     print '  Query (both):', ' '.join(parts)
+        #     print ''
+        #     print '  ' + ' '.join(
+        #         submission.segmentAndInsert(part, smoothCost, possibleFills)
+        #         for part in parts
+        #     )
+        #
+        # elif cmd == 'fills':
+        #     line = wordsegUtil.cleanLine(line)
+        #     print '\n'.join(possibleFills(line))
+        #
+        # elif cmd == 'ug':
+        #     line = wordsegUtil.cleanLine(line)
+        #     print unigramCost(line)
+        #
+        # elif cmd == 'bg':
+        #     grams = tuple(wordsegUtil.words(line))
+        #     prefix, ending = grams[-2], grams[-1]
+        #     print bigramCost(prefix, ending)
+
+        else:
+            print 'Unrecognized command:', cmd
+
+        print ''
+
+if __name__ == '__main__':
+
+    repl()
--- simulation.py
+++ simulation.py
@@ -3,11 +3,10 @@ import util
 import rpsBots
 
 class Simulation():
-    def __init__(self, bot1, bot2, rounds):
+    def __init__(self, bot1, bot2):
         """ ----------------------- """
         self.bot1 = bot1
         self.bot2 = bot2
-        self.rounds = rounds
         self.score = {'bot1': 0, 'bot2': 0, 'tie': 0}
         self.results = []
 
@@ -17,8 +16,8 @@ class Simulation():
     def simulate(self, rounds=None):
         self.reset() # reset the scores for new game
 
-        rounds = self.rounds if rounds == None else rounds
-        sys.stdout.write('Starting the %s-round simulation....' % rounds)
+        rounds = 1000 if rounds == None else rounds
+        sys.stdout.write('\tStarting the %s-round simulation....' % rounds)
         sys.stdout.flush()
 
         for _ in range(rounds):
@@ -29,7 +28,7 @@ class Simulation():
                                              self.bot2.playTie())]
             self.results.append(outcome)
             self.score[outcome] += 1
-        print 'Done!'
+        print 'Done!\n'
 
     def singleGame(self):
         sys.stdout.write('Starting the single game of simulation....\n\n')
@@ -43,15 +42,12 @@ class Simulation():
         play1 = self.bot1.playTurn()
         play2 = self.bot2.playTurn()
         outcome = util.CHECK_WINNER[(play1, play2)]
-        self.score[outcome] += 1
         print '\tPlayer 1 played: %s \t\tPlayer 2 played: %s' % (play1, play2)
         print '\t\t\tOutcome: %s' % cleanOutcome[outcome]
-        print '\nCurrent Score:'
-        print '\tPlayer 1: %s wins \t\tPlayer 2: %s wins' % (self.score['bot1'], self.score['bot2'])
 
     def __repr__(self):
         ''' overloading of print method for Simulation class '''
-        string = 'And the winner is...\n'
-        string += '\tBot1: %s\n' % self.score['bot1']
-        string += '\tBot2: %s\n' % self.score['bot2']
+        string = '\tAnd the winner is...\n'
+        string += '\t\tBot1: %s\n' % self.score['bot1']
+        string += '\t\tBot2: %s\n' % self.score['bot2']
         return string
